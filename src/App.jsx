e6385f825397095c2c6a76bcdda8606b1c5e56b3
E Abella <eabella@Es-MacBook-Air.local>
2024-07-22 04:55:00 -0700

    added test handlers

--- src/App.jsx
+++ src/App.jsx
@@ -1,36 +1,51 @@
-import { useEffect, useState } from 'react'
-import axios from 'axios';
+import { useState } from 'react'
 import './App.css'
-import e from 'express';
 
 
-const api_Key = {
+const api = {
   key: '6e06a1b430379e2e6e9895ab4c20dc09',
   base: 'https://api.openweathermap.org/data.2.5/',
 };
 
+
 function App() {
-  const [weatherData, setWeatherData] = useState([]);
-
-  const searchPressed = () => {
-    console.log('Search Pressed'); // test the search button returns this text in the console
-  }
-
-
   // hold state of search term using useState
   const [search, setSearch] = useState(''); // empty string
+  // hold the weather data
+  const [weather, setWeatherData] = useState({});
+
+
+  // TESTING THE SEARCH BUTTON
+  // const searchPressed = () => {
+  //   console.log('Search Pressed'); // test the search button returns this text in the console
+  //   console.log(search); // print the search term when the user types in a city 
+  // };
+
+  const searchPressed = () => {
+    if (search.trim() === '') {
+      console.error('Search term is empty');
+      return;
+    }
+
+    fetch(`${api.base}weather?q=${search}&units=metric&&APPID=${api.key}`) // specify api key and use search query
+    //get the results back and return a json object using the json method
+      .then((res) => res.json())
+      .then((result) => {
+        setWeatherData(result);
+      });
+};
   
-  // make api call to weather 
-  async function getWeatherData() {
+  // // make api call to weather 
+  // async function getWeatherData() {
 
-    //make news api call to using axios
-    const res = await axios.get('https://api.openweathermap.org/data/2.5/weather?lat={lat}&lon={lon}&appid={API key}');
-    setWeatherData(res.data.weather);
-  }
+  //   //make news api call to using axios
+  //   const res = await axios.get('https://api.openweathermap.org/data/2.5/weather?lat={lat}&lon={lon}&appid={api_Key}');
+  //   setWeatherData(res.data.weather);
+  // }
 
-  useEffect(() => {
-    getWeatherData();
-  }, []);
+  // useEffect(() => {
+  //   getWeatherData();
+  // }, []);
 
   return (
     <div> 
@@ -47,10 +62,11 @@ function App() {
       </div>
 
       {/* Location */}
-      <p> Location </p>
+      {/* the city that the user searches */}
+      <p> {weather.name} </p>  
 
       {/*  Condition */}
-      <p> Sunny </p>
+      <p> {weather.weather[0].main} </p>
     </div>
   );
 }
